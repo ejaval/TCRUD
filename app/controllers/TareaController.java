560f1434430391c5bbcf421b7aa282ed97a4e708
EDWIN JAVIER AREVALO LAZO <SMIS073521@ugb.edu.sv>
2025-03-25 15:05:27 -0600

    actualizaciones

--- app/controllers/TareaController.java
+++ app/controllers/TareaController.java
@@ -1,37 +1,45 @@
 package controllers;
 
 import models.Tarea;
+import play.api.i18n.Messages;
 import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Http;
-
+import play.i18n.MessagesApi;
 
 import javax.inject.Inject;
 import java.util.List;
 
 public class TareaController extends Controller {
     @Inject
-    private FormFactory formFactory;
+    private  FormFactory formFactory;
+    private final MessagesApi messagesApi;
 
-    //obtener las tareas
+    @Inject
+    public TareaController(FormFactory formFactory, MessagesApi messagesApi) {
+        this.formFactory = formFactory;
+        this.messagesApi = messagesApi;
+    }
+
+    // Obtener todas las tareas
     public Result index() {
         List<Tarea> tareas = Tarea.find.all();
-        return ok(views.html.tareas.index.render(tareas));
+            return ok(views.html.tareas.index.render(tareas));
     }
 
     //crear una nueva tarea
-    public Result create() {
+    public Result create(Http.Request request) {
         Form<Tarea> tareaForm = formFactory.form(Tarea.class);
-        return ok(views.html.tareas.crearTareas.render(tareaForm));
+        return ok(views.html.tareas.crearTareas.render(tareaForm, Messages.implicitMessagesProviderToMessages(messagesApi.preferred(request))));
     }
 
     //guardar una nueva tarea
     public Result save(Http.Request request) {
         Form<Tarea>  tareaForm = formFactory.form(Tarea.class).bindFromRequest(request);
         if(tareaForm.hasErrors()) {
-            return badRequest(views.html.tareas.crearTareas.render(tareaForm));
+            return badRequest(views.html.tareas.crearTareas.render(tareaForm, Messages.implicitMessagesProviderToMessages(messagesApi.preferred(request))));
         }
         Tarea tarea = tareaForm.get();
         tarea.save();
@@ -48,20 +56,20 @@ public class TareaController extends Controller {
     }
 
     //editar tareas
-    public Result edit(Long id) {
+    public Result edit(Long id, Http.Request request) {
         Tarea tarea = Tarea.find.byId(id);
         if (tarea == null) {
             return notFound("Tarea no encontrada");
         }
         Form<Tarea> tareaForm = formFactory.form(Tarea.class).fill(tarea);
-        return ok(views.html.tareas.editarTarea.render(tareaForm, id));
+        return ok(views.html.tareas.editarTarea.render(tareaForm, id, Messages.implicitMessagesProviderToMessages(messagesApi.preferred(request))));
     }
 
     //actualizar
     public Result update(Long id, Http.Request request) {
         Form<Tarea> tareaForm = formFactory.form(Tarea.class).bindFromRequest(request);
         if(tareaForm.hasErrors()) {
-            return badRequest(views.html.tareas.editarTarea.render(tareaForm, id));
+            return badRequest(views.html.tareas.editarTarea.render(tareaForm, id, Messages.implicitMessagesProviderToMessages(messagesApi.preferred(request))));
         }
         Tarea tarea = tareaForm.get();
         tarea.id = id;
